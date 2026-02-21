18de56558cd8971ac89eb12eb18d1e8ac5dd5333
behappyman <a18091986>
2026-02-21 13:52:31 +0300

    AsyncGenerator

--- 7/database.py
+++ 7/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-from typing import Annotated
+from typing import Annotated, AsyncGenerator
 from fastapi import Depends
 from config import config
 
@@ -12,7 +12,7 @@ class Base(DeclarativeBase):
     pass
 
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
 
